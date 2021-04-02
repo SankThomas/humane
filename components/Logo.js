5437d384e630657bbbb937011a9d4155e7cc4e10
Thomas Sankara <thomassankara74@gmail.com>
2021-04-02 07:48:02 +0300

    Logo component

---
+++ components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "../styles/Logo.module.css"
+
+const Logo = () => {
+  return (
+    <>
+      <h2 className={styles.logo}>Humane</h2>
+    </>
+  )
+}
+
+export default Logo
